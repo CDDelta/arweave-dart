f7e1ebe2fe6ff9e52f28c68fdd3c2e14dacf5d35
CDDelta <ngjunyen@gmail.com>
2020-09-08 22:06:29 +1000

    simplify deep hash implementation

--- lib/src/crypto/deep_hash.dart
+++ lib/src/crypto/deep_hash.dart
@@ -5,22 +5,22 @@ import 'package:crypto/crypto.dart';
 
 Future<Uint8List> deepHash(List<Object> data) async {
   final tag = utf8.encode('list') + utf8.encode(data.length.toString());
-  return _deepHashChunks(data, 0, sha384.convert(tag).bytes);
+  return _deepHashChunks(data, sha384.convert(tag).bytes);
 }
 
 Future<Uint8List> _deepHashChunks(
-    List<Object> chunks, int i, Uint8List acc) async {
+    Iterable<Object> chunks, Uint8List acc) async {
   // If we're at the end of the chunks list, return.
-  if (i >= chunks.length - 1) return acc;
+  if (chunks.isEmpty) return acc;
 
   final hashPair = acc +
-      // If the current chunk is not a byte list, we assume it's an object list.
-      (chunks[i] is! Uint8List
-          ? await deepHash(chunks[i])
-          : await _deepHashChunk(chunks[i]));
-  final newAcc = sha384.convert(hashPair).bytes;
+      // If the current chunk is not a byte list, we assume it's a nested byte list.
+      (chunks.first is! Uint8List
+          ? await deepHash(chunks.first)
+          : await _deepHashChunk(chunks.first));
 
-  return _deepHashChunks(chunks, i + 1, newAcc);
+  final newAcc = sha384.convert(hashPair).bytes;
+  return _deepHashChunks(chunks.skip(1), newAcc);
 }
 
 Future<Uint8List> _deepHashChunk(Uint8List data) async {
