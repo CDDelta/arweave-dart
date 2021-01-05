16712a5c3e89d792a7108c1ac77e2779282ac24f
CDDelta <ngjunyen@gmail.com>
2021-01-05 16:40:49 +1100

    refactor: remove wallet info api

--- lib/src/arweave.dart
+++ lib/src/arweave.dart
@@ -1,14 +1,10 @@
 import 'api/api.dart';
 import 'chunks.dart';
 import 'transactions.dart';
-import 'wallets.dart';
 
 class Arweave {
   ArweaveApi get api => _api;
   ArweaveApi _api;
-
-  ArweaveWalletsApi get wallets => _wallets;
-  ArweaveWalletsApi _wallets;
 
   ArweaveTransactionsApi get transactions => _transactions;
   ArweaveTransactionsApi _transactions;
@@ -20,7 +16,6 @@ class Arweave {
     Uri gatewayUrl,
   }) {
     _api = ArweaveApi(gatewayUrl: gatewayUrl);
-    _wallets = ArweaveWalletsApi(api);
     _transactions = ArweaveTransactionsApi(api);
     _chunks = ArweaveChunksApi(api);
   }
--- test/wallets_test.dart
+++ test/wallets_test.dart
@@ -7,13 +7,7 @@ import 'package:test/test.dart';
 
 import 'utils.dart';
 
-final liveAddressBalance = BigInt.from(498557055636);
-const liveAddress = '9_666Wkk2GzL0LGd3xhb0jY7HqNy71BaV4sULQlJsBQ';
-const liveTxid = 'CE-1SFiXqWUEu0aSTebE6LC0-5JBAc3IAehYGwdF5iI';
-
 void main() {
-  final client = Arweave();
-
   group('wallets:', () {
     test('decode and encode wallet', () async {
       final jwk =
@@ -38,8 +32,8 @@ void main() {
 
     final jwkFieldPattern = RegExp(r'^[a-z0-9-_]{683}$', caseSensitive: false);
     test('generate wallet', () async {
-      final walletA = await client.wallets.generate();
-      final walletB = await client.wallets.generate();
+      final walletA = await Wallet.generate();
+      final walletB = await Wallet.generate();
 
       final walletJwk = walletA.toJwk();
       expect(walletJwk['kty'], equals('RSA'));
@@ -52,22 +46,6 @@ void main() {
       expect(walletB.address, matches(digestPattern));
 
       expect(walletA.address, isNot(equals(walletB.address)));
-    });
-
-    test('get wallet info', () async {
-      expect(await client.wallets.getBalance(liveAddress),
-          equals(liveAddressBalance));
-      expect(await client.wallets.getLastTransactionId(liveAddress),
-          equals(liveTxid));
-
-      final fakeWallet = await getTestWallet();
-
-      expect(await client.wallets.getBalance(fakeWallet.address),
-          equals(BigInt.zero));
-      expect(await client.wallets.getLastTransactionId(fakeWallet.address),
-          equals(''));
-    }, onPlatform: {
-      'browser': Skip('dart:io unavailable'),
     });
 
     test('resolve address from wallet', () async {
