7b3e7567b5de1a6dcffe1c14f6d99d81ffc888b5
jdaev <jdhn42@gmail.com>
2021-11-22 08:44:29 -0500

    task: undo data bundle changes

--- lib/src/models/data-bundle.dart
+++ lib/src/models/data-bundle.dart
@@ -11,28 +11,26 @@ class DataBundle {
 
   Future<Uint8List> asBlob() async {
     final headers = Uint8List(64 * items.length);
-    final dataItemBinaries = <Uint8List>[];
-    items.forEach((d) async {
-      final binary = await () async {
+    final binaries = await Future.wait(
+      items.map((d) async {
         // Sign DataItem
         var index = items.indexOf(d);
         final id = decodeBase64ToBytes(d.id);
         // Create header array
         final header = Uint8List(64);
-        final rawDataItem = await d.asBinary();
+        final raw = await d.asBinary();
         // Set offset
-        header.setAll(0, longTo32ByteArray(rawDataItem.lengthInBytes));
+        header.setAll(0, longTo32ByteArray(raw.lengthInBytes));
         // Set id
         header.setAll(32, id);
         // Add header to array of headers
         headers.setAll(64 * index, header);
         // Convert to array for flattening
-        return rawDataItem.asUint8List();
-      }();
-      dataItemBinaries.add(binary);
+        return raw.asUint8List();
+      }),
+    ).then((a) {
+      return a.reduce((a, e) => Uint8List.fromList(a + e));
     });
-    final binaries =
-        dataItemBinaries.reduce((a, e) => Uint8List.fromList(a + e));
     final buffer = Uint8List.fromList([
       ...longTo32ByteArray(items.length),
       ...headers,
