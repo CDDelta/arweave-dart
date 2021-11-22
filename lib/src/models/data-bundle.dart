e4c5c710d3b8240bd7264c9bb1967777032582dd
jdaev <jdhn42@gmail.com>
2021-11-22 10:05:13 -0500

    Revert "task: add prepareDataItems function"
    This reverts commit 4d2ac900c2fb5934545fb1a9cf25596a6f51600e.

--- lib/src/models/data-bundle.dart
+++ lib/src/models/data-bundle.dart
@@ -6,57 +6,31 @@ import '../../utils.dart';
 
 class DataBundle {
   final List<DataItem> items;
-  final Map<Uint8List, ByteBuffer> dataItemBinaries = {};
 
   DataBundle({required this.items});
 
-  Future<void> prepareDataItem(DataItem item) async {
-    final id = decodeBase64ToBytes(item.id);
-    final raw = await item.asBinary();
-    dataItemBinaries.putIfAbsent(id, () => raw);
-  }
-
   Future<Uint8List> asBlob() async {
     final headers = Uint8List(64 * items.length);
-    final binaries = dataItemBinaries.isEmpty
-        ? await Future.wait(
-            items.map((d) async {
-              // Sign DataItem
-              var index = items.indexOf(d);
-              final id = decodeBase64ToBytes(d.id);
-              // Create header array
-              final header = Uint8List(64);
-              final raw = await d.asBinary();
-              // Set offset
-              header.setAll(0, longTo32ByteArray(raw.lengthInBytes));
-              // Set id
-              header.setAll(32, id);
-              // Add header to array of headers
-              headers.setAll(64 * index, header);
-              // Convert to array for flattening
-              return raw.asUint8List();
-            }),
-          ).then((a) {
-            return a.reduce((a, e) => Uint8List.fromList(a + e));
-          })
-        : dataItemBinaries.keys.map((id) {
-            // Sign DataItem
-            var index = dataItemBinaries.keys.toList().indexOf(id);
-
-            // Create header array
-            final header = Uint8List(64);
-            final raw = dataItemBinaries[id]!;
-            // Set offset
-            header.setAll(0, longTo32ByteArray(raw.lengthInBytes));
-            // Set id
-            header.setAll(32, id);
-            // Add header to array of headers
-            headers.setAll(64 * index, header);
-            // Convert to array for flattening
-            return raw.asUint8List();
-          }).reduce(
-            (a, e) => Uint8List.fromList(a + e),
-          );
+    final binaries = await Future.wait(
+      items.map((d) async {
+        // Sign DataItem
+        var index = items.indexOf(d);
+        final id = decodeBase64ToBytes(d.id);
+        // Create header array
+        final header = Uint8List(64);
+        final raw = await d.asBinary();
+        // Set offset
+        header.setAll(0, longTo32ByteArray(raw.lengthInBytes));
+        // Set id
+        header.setAll(32, id);
+        // Add header to array of headers
+        headers.setAll(64 * index, header);
+        // Convert to array for flattening
+        return raw.asUint8List();
+      }),
+    ).then((a) {
+      return a.reduce((a, e) => Uint8List.fromList(a + e));
+    });
     final buffer = Uint8List.fromList([
       ...longTo32ByteArray(items.length),
       ...headers,
