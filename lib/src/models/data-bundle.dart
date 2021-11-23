cfa089b3c5a7a33c81c125e1e557b4383244e1fc
jdaev <jdhn42@gmail.com>
2021-11-23 11:13:56 -0500

    task: fix future not running PE-572

--- lib/src/models/data-bundle.dart
+++ lib/src/models/data-bundle.dart
@@ -13,24 +13,23 @@ class DataBundle {
     final headers = Uint8List(64 * items.length);
     // Use precalculated buffers if provided to
     final binaries = BytesBuilder();
-    await Future.wait(
-      items.map((d) async {
-        // Sign DataItem
-        var index = items.indexOf(d);
-        final id = decodeBase64ToBytes(d.id);
-        // Create header array
-        final header = Uint8List(64);
-        final raw = await d.asBinary();
-        // Set offset
-        header.setAll(0, longTo32ByteArray(raw.length));
-        // Set id
-        header.setAll(32, id);
-        // Add header to array of headers
-        headers.setAll(64 * index, header);
-        // Convert to array for flattening
-        binaries.add(raw.takeBytes());
-      }),
-    );
+    items.forEach((item) async {
+      // Sign DataItem
+      var index = items.indexOf(item);
+      final id = decodeBase64ToBytes(item.id);
+      // Create header array
+      final header = Uint8List(64);
+      final raw = await item.asBinary();
+      // Set offset
+      header.setAll(0, longTo32ByteArray(raw.length));
+      // Set id
+      header.setAll(32, id);
+      // Add header to array of headers
+      headers.setAll(64 * index, header);
+      // Convert to array for flattening
+      binaries.add(raw.takeBytes());
+    });
+
     final buffer = BytesBuilder();
     buffer.add([
       ...longTo32ByteArray(items.length),
