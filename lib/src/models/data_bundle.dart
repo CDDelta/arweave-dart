d1556cadae18895c29011c670b4612b12626badc
Javed Hussein <jdhn42@gmail.com>
2022-01-19 12:12:24 -0500

    fix: correct data item indexes

--- lib/src/models/data_bundle.dart
+++ lib/src/models/data_bundle.dart
@@ -39,15 +39,15 @@ class DataBundle {
   }
 
   static Future<DataBundle> fromHandles({
-    required List<DataItemHandle> items,
+    required List<DataItemHandle> handles,
   }) async {
-    final headers = Uint8List(64 * items.length * 2);
+    final headers = Uint8List(64 * handles.length * 2);
     // Use precalculated buffers if provided to
+    var dataItemIndex = 0;
     final binaries = BytesBuilder();
-    await Future.wait(items.map((item) async {
+    await Future.wait(handles.map((handle) async {
       // Sign DataItem
-      var index = items.indexOf(item);
-      final dataItems = await item.createDataItemsFromFileHandle();
+      final dataItems = await handle.createDataItemsFromFileHandle();
       assert(dataItems.length == 2);
       for (var dataItem in dataItems) {
         final id = decodeBase64ToBytes(dataItem.id);
@@ -60,14 +60,15 @@ class DataBundle {
         // Set id
         header.setAll(32, id);
         // Add header to array of headers
-        headers.setAll(64 * index, header);
+        headers.setAll(64 * dataItemIndex, header);
+        dataItemIndex++;
         // Convert to array for flattening
         binaries.add(raw.takeBytes());
       }
     }));
 
     final buffer = BytesBuilder();
-    buffer.add(longTo32ByteArray(items.length * 2));
+    buffer.add(longTo32ByteArray(handles.length * 2));
     buffer.add(headers);
     buffer.add(binaries.takeBytes());
     return DataBundle(blob: buffer.takeBytes());
