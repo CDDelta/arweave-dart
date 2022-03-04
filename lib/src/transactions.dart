be5ec813d138f135823960ff64921080b45388cb
CDDelta <me@cddelta.dev>
2022-03-05 07:25:45 +1100

    feat: parallelise transaction chunk uploading (#17)
    Refactors the `TransactionUploader` implementation to parallelize the seeding of transaction chunks. Works by initiating `maxConcurrentChunkUploadCount` seeding requests (128 chunks * 256 KB = ~32 MB) to the Arweave gateway at once and starts up new seeding requests individually as old operations complete to better saturate client bandwidth over batching.

--- lib/src/transactions.dart
+++ lib/src/transactions.dart
@@ -69,21 +69,26 @@ class ArweaveTransactionsApi {
   /// Returns an uploader than can be used to upload a transaction chunk by chunk, giving progress
   /// and the ability to resume.
   Future<TransactionUploader> getUploader(Transaction transaction,
-          {bool forDataOnly = false}) async =>
-      TransactionUploader(transaction, _api, forDataOnly: forDataOnly);
+          {int maxConcurrentUploadCount = 128,
+          bool forDataOnly = false}) async =>
+      TransactionUploader(transaction, _api,
+          maxConcurrentChunkUploadCount: maxConcurrentUploadCount,
+          forDataOnly: forDataOnly);
 
   /// Uploads the transaction in full, returning a stream of events signaling the status of the upload.
   Stream<TransactionUploader> upload(
     Transaction transaction, {
+    int maxConcurrentUploadCount = 128,
     bool dataOnly = false,
     bool dryRun = false,
   }) async* {
-    final uploader = await getUploader(transaction, forDataOnly: dataOnly);
+    final uploader = await getUploader(transaction,
+        maxConcurrentUploadCount: maxConcurrentUploadCount,
+        forDataOnly: dataOnly);
 
-    while (!uploader.isComplete) {
-      if (!dryRun) {
-        await uploader.uploadChunk();
-      }
+    if (!dryRun) {
+      yield* uploader.upload();
+    } else {
       yield uploader;
     }
   }
@@ -91,14 +96,11 @@ class ArweaveTransactionsApi {
   /// Uploads the transaction in full. Useful for small data or wallet transactions.
   Future<void> post(
     Transaction transaction, {
+    int maxConcurrentUploadCount = 128,
     bool dryRun = false,
   }) async {
-    if (dryRun) {
-      return;
-    }
-    final uploader = await getUploader(transaction);
-    while (!uploader.isComplete) {
-      await uploader.uploadChunk();
-    }
+    await upload(transaction,
+            maxConcurrentUploadCount: maxConcurrentUploadCount, dryRun: dryRun)
+        .drain();
   }
 }
