e4733b2cc92a535c806f67d449a3c8f1a855f3fa
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2022-10-12 14:13:20 -0300

    use for each approach instead of wait

--- lib/src/models/data_bundle.dart
+++ lib/src/models/data_bundle.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:typed_data';
 
 import 'package:arweave/arweave.dart';
@@ -42,6 +43,9 @@ class DataBundle {
   static Future<DataBundle> fromHandles({
     required List<DataItemHandle> handles,
   }) async {
+    log('Get DataBundle from handles...');
+    log('Number of handles: ${handles.length}');
+
     final noOfDataItems = handles
         .map((handle) => handle.dataItemCount)
         .reduce((sum, current) => sum += current);
@@ -49,35 +53,63 @@ class DataBundle {
     // Use precalculated buffers if provided to
     var dataItemIndex = 0;
     final binaries = BytesBuilder();
-    await Future.wait(
-      handles.map(
-        (handle) async {
-          // Sign DataItem
-          final dataItems = await handle.getDataItems();
-          for (var dataItem in dataItems) {
-            final id = decodeBase64ToBytes(dataItem.id);
-            // Create header array
-            final header = Uint8List(64);
-            final raw = await dataItem.asBinary();
-            dataItem.data = Uint8List(0);
-            // Set offset
-            header.setAll(0, longTo32ByteArray(raw.length));
-            // Set id
-            header.setAll(32, id);
-            // Add header to array of headers
-            headers.setAll(64 * dataItemIndex, header);
-            dataItemIndex++;
-            // Convert to array for flattening
-            binaries.add(raw.takeBytes());
-          }
-        },
-      ),
-    );
+
+    await Future.forEach<DataItemHandle>(handles, (handle) async {
+      log('current ${handle.hashCode}');
+      // Sign DataItem
+      log('Getting DataItems');
+
+      final dataItems = await handle.getDataItems();
+
+      log('DataItems done');
+      log('Number of data items: ${dataItems.length}');
+
+      log('Iterating for each DataItem');
+
+      for (var dataItem in dataItems) {
+        log('Current dataItem: ${dataItem.id}');
+
+        log('Decoding');
+
+        final id = decodeBase64ToBytes(dataItem.id);
+
+        log('Decoded');
+
+        // Create header array
+        final header = Uint8List(64);
+
+        log('Get raw');
+
+        final raw = await dataItem.asBinary();
+
+        log('Raw done: ${raw.length}');
+
+        dataItem.data = Uint8List(0);
+        // Set offset
+        header.setAll(0, longTo32ByteArray(raw.length));
+        // Set id
+        header.setAll(32, id);
+        // Add header to array of headers
+        headers.setAll(64 * dataItemIndex, header);
+        dataItemIndex++;
+        // Convert to array for flattening
+        binaries.add(raw.takeBytes());
+
+        log('Added binaries from ${dataItem.id}');
+      }
+    });
+
+    log('DataBundle from handles finished');
+    log('Mouting buffer....');
 
     final buffer = BytesBuilder();
     buffer.add(longTo32ByteArray(noOfDataItems));
     buffer.add(headers);
     buffer.add(binaries.takeBytes());
+
+    log('Buffer mounted');
+    log('Returning DataBundle');
+
     return DataBundle(blob: buffer.takeBytes());
   }
 }
