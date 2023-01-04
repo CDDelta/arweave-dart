910dc058cb37a7c902edd09eb355acf8f597ec69
Elliot Sayes <elliot@sayes.cloud>
2023-01-04 12:14:16 +0800

    Additional test for stream TransactionUploader

--- test/transactions_test.dart
+++ test/transactions_test.dart
@@ -276,6 +276,22 @@ void main() {
         'browser': Skip('dart:io unavailable'),
       });
 
+      test('successfully seed existing network transaction', () async {
+        final transaction = await client.transactions
+            .get<TransactionStream>('8C6yYu5pWMADLSd65wTnrzgN-9eLj9sFbyVC3prSaFs');
+
+        final data = utf8.encode('{"name":"Blockchains & Cryptocurrencies"}') as Uint8List;
+        await transaction!.setDataStreamGenerator(() => Stream.value(data), data.length);
+
+        expect(
+          client.transactions.upload(transaction, dataOnly: true),
+          emitsInOrder([
+            emits(anything),
+            emitsDone,
+          ]),
+        );
+      });
+
       test('successfully seed existing large network transaction', () async {
         final txId = 'gAnkEioD7xoP3qx7VepVEp1O0v4L1UgtBV_trM-Ria8';
         final transaction = await client.transactions.get<TransactionStream>(txId);
