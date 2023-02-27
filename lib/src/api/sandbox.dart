8a9b0f58a225f562a3409dc2adf3420c11ea53f1
Mati <matibat@gmail.com>
2023-02-27 13:07:00 -0300

    feat(sandbox): ovewrites the implementation PE-3039

--- lib/src/api/sandbox.dart
+++ lib/src/api/sandbox.dart
@@ -1,24 +1,37 @@
+import 'dart:convert' as convert;
 import 'dart:typed_data';
 
 import 'package:base32/base32.dart';
 
 String getSandboxSubdomain(String txId) {
-  return toB32(fromB64Url(txId));
+  final bytes = b64UrlToBytes(txId);
+  final b32Encoded = bytesToB32String(bytes);
+  return b32Encoded;
 }
 
-Uint8List fromB64Url(String input) {
-  int paddingLength = (input.length % 4 == 0 ? 0 : 4 - (input.length % 4));
-
-  var base64 = input.replaceAll("-", "+").replaceAll("_", "/");
-  base64 += ("=" * paddingLength);
-
-  return _base64ToArrayBuffer(base64);
+Uint8List b64UrlToBytes(String base64Url) {
+  final base64 = b64UrlToB64(base64Url);
+  final bytes = b64ToBytes(base64);
+  return bytes;
 }
 
-String toB32(Uint8List input) {
+String b64UrlToB64(String base64Url) {
+  final paddingLength = nextMultipleOf4(base64Url.length) - base64Url.length;
+  var b64Encoded = base64Url.replaceAll("-", "+").replaceAll("_", "/");
+  b64Encoded += ("=" * paddingLength);
+  return b64Encoded;
+}
+
+Uint8List b64ToBytes(String base64) {
+  final paddingLength = nextMultipleOf4(base64.length) - base64.length;
+  final bytes = convert.base64.decode('$base64${'=' * paddingLength}');
+  return bytes;
+}
+
+String bytesToB32String(Uint8List bytes) {
   final base32String = base32
       .encode(
-        input,
+        bytes,
       )
       .replaceAll('=', '')
       .toLowerCase();
@@ -26,42 +39,11 @@ String toB32(Uint8List input) {
   return base32String;
 }
 
-Uint8List _base64ToArrayBuffer(base64) {
-  var binaryString = atobPolyfill(base64);
-
-  var len = binaryString.length;
-
-  var bytes = Uint8List(len);
-
-  for (var i = 0; i < len; i++) {
-    bytes[i] = binaryString.codeUnitAt(i);
+int nextMultipleOf4(int n) {
+  final remainder = n % 4;
+  if (remainder == 0) {
+    return n;
+  } else {
+    return n + (4 - remainder);
   }
-
-  return bytes;
-}
-
-String atobPolyfill(input) {
-  var chars =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-  var str = input.replaceAll(RegExp(r'[=]+$'), "");
-  if (str.length % 4 == 1) {
-    return input;
-  }
-  var output = "";
-  // TODO: is null check appropriate?
-  for (var bc = 0, bs, buffer, idx = 0; idx < str.length; idx++) {
-    buffer = str[idx];
-    buffer = chars.indexOf(buffer);
-    if (~buffer != 0) {
-      if (bc % 4 > 0) {
-        bs = bs * 64 + buffer;
-        bc++;
-        output += String.fromCharCode(255 & (bs >> ((-2 * bc) & 6)));
-      } else {
-        bs = buffer;
-        bc++;
-      }
-    }
-  }
-  return output;
 }
