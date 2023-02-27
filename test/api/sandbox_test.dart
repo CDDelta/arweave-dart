29bfd003bca6dee8083ce6fab945f9c4edc0f8f1
Mati <matibat@gmail.com>
2023-02-27 14:03:56 -0300

    test(sandbox): test multiple cases PE-3039

--- test/api/sandbox_test.dart
+++ test/api/sandbox_test.dart
@@ -5,27 +5,17 @@ import 'package:test/test.dart';
 
 import 'sandbox_test_utils.dart' as sandbox_test_utils;
 
-/// TODO: update the test cases
-// typedef SandboxDomain = String;
-// typedef TxId = String;
-// final Map<SandboxDomain, TxId> testBaseline = {
-//   'f3valm4d36fd6w4uhuibvjrnwra3entolqiojhlcsghhnp32pstq':
-//       'LuoFs4Pfij9blD0QGqYttEGyNm5cEOSdYpGOdr96fKc',
-//   'flqlektwzxrgtlpmce53dnnej2clq5njgbbh2tztjbs4aqz3owjq':
-//       'KuCyKnbN4mmt7BE7sbWkToS4dakwQn1PM0hlwEM7dZM',
-//   '6c6fodlctydufzmbsmzykkweigtk5w5zrqauiyetmcaqukgm':
-//       '8LxXDWKeB_0LlgZMzhSrEQaau27-mMAURgk2CBCijMY',
-//   'oerew5mrlsda5yrsmml3vwpxb6tvpobbzyfb4l5pucthyyhmbe':
-//       'cSJLdZFchg7iMmMXutn3D6dXuCHOCh4_vr6CmfGDsCc',
-//   'vxxdwy6scapybz7ekkw32ua5naseioagnu4uutbq3cftvivl':
-//       're47Y9IQH4-Dn5FKtvVAdaCRE-OAZtOUpMMNiLOqKrw',
-//   'bujjxactpuy7cmm3ul6fjuwuqnbymloddqvsjswhxsvkd255bq':
-//       'DRKbgFN9MfExm6L8VNLUg0OGLcMcKyTKx7yq_oeu9DM',
-//   'b4zy2sx2upeczfaxpscxfuglgh2mljo4gu6ycuhujgz5yjbc':
-//       'DzONSvqjyCyUF3yFctDLMfTFpd_w-1PYFQ9Emz3CQic',
-//   'kruhyqr3j24gxknxda2pbqa6ru2ctocctwr4z3hesnjukreb':
-//       'VGh8QjtOuGuptxg08MAejTQpuEKdo8zs_5JNTRUSB_Q',
-// };
+typedef TxId = String;
+final List<TxId> validTxIds = [
+  'LuoFs4Pfij9blD0QGqYttEGyNm5cEOSdYpGOdr96fKc',
+  'KuCyKnbN4mmt7BE7sbWkToS4dakwQn1PM0hlwEM7dZM',
+  '8LxXDWKeB_0LlgZMzhSrEQaau27-mMAURgk2CBCijMY',
+  'cSJLdZFchg7iMmMXutn3D6dXuCHOCh4_vr6CmfGDsCc',
+  're47Y9IQH4-Dn5FKtvVAdaCRE-OAZtOUpMMNiLOqKrw',
+  'DRKbgFN9MfExm6L8VNLUg0OGLcMcKyTKx7yq_oeu9DM',
+  'DzONSvqjyCyUF3yFctDLMfTFpd_w-1PYFQ9Emz3CQic',
+  'VGh8QjtOuGuptxg08MAejTQpuEKdo8zs_5JNTRUSB_Q',
+];
 
 void main() {
   group('b64UrlToB64 method', () {
@@ -101,10 +91,11 @@ void main() {
 
   group('getSandboxedSubdomain method', () {
     test('should return the correct sandbox subdomain', () {
-      final input = 'x0r3HMmsWeoicM81PFQI9DONnUt-XjLgEKK3DsoDL_M';
-      final expected = sandbox_test_utils.getSandboxSubdomain(input);
-      final actual = sandbox.getSandboxSubdomain(input);
-      expect(actual, expected);
+      for (var input in validTxIds) {
+        final expected = sandbox_test_utils.getSandboxSubdomain(input);
+        final actual = sandbox.getSandboxSubdomain(input);
+        expect(actual, expected);
+      }
     });
   });
 }
